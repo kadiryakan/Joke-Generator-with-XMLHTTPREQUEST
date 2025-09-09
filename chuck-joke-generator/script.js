1440650777a2bf4ab2f284a57e31849ffcac077d
Kadir Yakan <kadiryakan9@gmail.com>
2025-09-09 18:48:45 +0300

    Random joke project written with XMLHTTPREQUEST

---
+++ chuck-joke-generator/script.js
@@ -0,0 +1,24 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+
+const generateJoke = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random1");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        jokeEl.innerHTML = JSON.parse(this.responseText).value;
+      } else {
+        jokeEl.style.color = "red";
+        jokeEl.style.fontWeight = "bold";
+        jokeEl.innerHTML =
+          "AIGHT LIL BRO I KNOW THIS ISNT FUNNY JUST SHUT YO MOUTH DAMN";
+      }
+    }
+  };
+  xhr.send();
+};
+jokeBtn.addEventListener("click", generateJoke);
+document.addEventListener("DOMContentLoaded", generateJoke);
